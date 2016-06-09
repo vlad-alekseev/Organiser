1f5f7196f91380c3bb99fb859c2a2db83b94b377
vlad-alekseev <vlad.alexeev.1990@gmail.com>
2016-06-09 12:06:27 +0300

    update

--- app/src/main/java/com/organiser/receiver/AlarmReceiver.java
+++ app/src/main/java/com/organiser/receiver/AlarmReceiver.java
@@ -5,7 +5,6 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.organiser.activity.AlarmActivity;
 
@@ -14,7 +13,6 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("vlad123", " start alarm");
         Intent intentAlarm = new Intent(context, AlarmActivity.class);
         intentAlarm.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.getApplicationContext().startActivity(intentAlarm);
